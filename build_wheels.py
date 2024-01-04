8912b62d0dab98c943e76eae89a8a7589f3d5033
Zach Kanzler <they4kman@gmail.com>
2024-01-04 16:22:47 -0500

    fix(ops): appease the gotdamn pypa platform tag gods

--- build_wheels.py
+++ build_wheels.py
@@ -16,15 +16,15 @@ def build(setup_kwargs):
     sys.path.insert(0, str(egg_base / 'lib'))
     from vacuum_downloader import ensure_vacuum_binary_installed
 
-    for plat, arch in [
-        ('darwin', 'arm64'),
-        ('darwin', 'x86_64'),
-        ('linux', 'arm64'),
-        ('linux', 'i386'),
-        ('linux', 'x86_64'),
-        ('windows', 'arm64'),
-        ('windows', 'i386'),
-        ('windows', 'x86_64'),
+    for plat, arch, bdist_plat in [
+        # Disable until macosx platform tags are understood
+        # ('darwin', 'arm64'),
+        # ('darwin', 'x86_64'),
+        ('linux', 'arm64', 'manylinux2014_aarch64'),
+        ('linux', 'i386', 'manylinux2014_i686'),
+        ('linux', 'x86_64', 'manylinux2014_x86_64'),
+        ('windows', 'i386', 'win32'),
+        ('windows', 'x86_64', 'win_amd64'),
     ]:
         ensure_vacuum_binary_installed(plat, arch, force=True)
 
@@ -33,7 +33,7 @@ def build(setup_kwargs):
             script_args=['bdist_wheel'],
             options={
                 'bdist_wheel': {
-                    'plat_name': f'{plat}_{arch}',
+                    'plat_name': bdist_plat,
                     'bdist_dir': f'{egg_base}/lib',
                     'keep_temp': True,
                 },
